a81352ab3a660b781822a86dc89dc9147628aeb7
abhinav <abhinav.kumar11o4@gmail.com>
2022-09-16 17:31:45 +0530

    added colors.kt file

---
+++ common/src/commonMain/kotlin/com/ode/code/common/ui/theme/colors.kt
@@ -0,0 +1,27 @@
+package com.ode.code.common.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Body color
+val MainBg = Color(0xFFF5ECDD)
+val white = Color( 0xFFFFFFFF)
+val offwhite = Color( 0xFFF5F5F5)
+val lightblue = Color(0xFF4F9FDB)
+val blue = Color(0xFF20B5E4)
+val DarkBlue = Color(0xFF184895)
+val ShadowColor = Color(0x80bfafb2)
+
+//Primary color
+val orange = Color(0xFFFF6900)
+val orangelight = Color(0xFFFF8A00)
+val card1bg = Color(0xFFFFF9CA)
+val card2bg = Color(0xFFFFDEB4)
+val card3bg = Color(0xFFB2A4FF)
+val card4bg = Color(0xFFFFB4B4)
+val card5bg = Color(0xFFCDF0EA)
+val card6bg = Color(0xFFF6C6EA)
+
+
+//Text color
+val Text200 = Color(0xFFB7B4B4)
+val Text600 = Color(0xFF000000)
